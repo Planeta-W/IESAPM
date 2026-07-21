5a9fb2841971f68bf219cb30b21b4a5d3abda368
Alexandre Scrideli <alexandrescrideli@gmail.com>
2026-07-21 12:18:55 -0300

    refactor: incluindo gtag em header

--- themes/iesapm/header.php
+++ themes/iesapm/header.php
@@ -13,11 +13,23 @@
 
 <head>
 
-   <?php // Google tag (gtag.js)
-   echo do_shortcode( '[wpcode id="2927"]' ); ?>
+   <!-- Google tag (gtag.js) -->
+   <script async src="https://www.googletagmanager.com/gtag/js?id=G-5T9K85DKYH"></script>
+   <script>
+   window.dataLayer = window.dataLayer || [];
+   function gtag(){dataLayer.push(arguments);}
+   gtag('js', new Date());
 
-   <?php // Google Tag Manager
-   echo do_shortcode( '[wpcode id="2754"]' ); ?>
+   gtag('config', 'G-5T9K85DKYH');
+   </script>
+
+   <!-- Google Tag Manager -->
+   <script>(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+   new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+   j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+   'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+   })(window,document,'script','dataLayer','GTM-WDCTMTR');</script>
+   <!-- End Google Tag Manager -->
 
    <meta charset="UTF-8">
    <meta name="viewport" content="width=device-width, initial-scale=1.0">
@@ -28,8 +40,10 @@
 
 <body <?php body_class(); ?>>
 
-<?php // Google Tag Manager (noscript)
-echo do_shortcode( '[wpcode id="2755"]' ); ?>
+<!-- Google Tag Manager (noscript) -->
+<noscript><iframe src="https://www.googletagmanager.com/ns.html?id=GTM-WDCTMTR"
+height="0" width="0" style="display:none;visibility:hidden"></iframe></noscript>
+<!-- End Google Tag Manager (noscript) -->
 
 <?php 
 // POPUP
