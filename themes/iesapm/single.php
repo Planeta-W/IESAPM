4e454d92c8ae2fbd7717312bbfe942b02d6e1366
Alexandre Scrideli <alexandrescrideli@gmail.com>
2026-08-04 15:12:29 -0300

    fix: corrigindo single cursos

--- themes/iesapm/single.php
+++ themes/iesapm/single.php
@@ -1,5 +1,5 @@
 <?php
-if(is_singular(array('graduacao', 'pos-graduacao', 'extensao', 'cursos-livres'))):
+if(is_singular(array('graduacao', 'pos-graduacao', 'extensao', 'cursos_livres'))):
 
 get_template_part('singles/single-curso');
 
